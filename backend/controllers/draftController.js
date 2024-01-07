7662ad96ae319ba4b36404fd3f00ea196cf0bf53
maks <yahnyshchak@gmail.com>
2024-01-07 19:46:45 +0000

    Added api routes controllers, such as: create, delete, update, get draft/drafts.

---
+++ backend/controllers/draftController.js
@@ -0,0 +1,79 @@
+const Drafts = require('../models/draftModel'); 
+const mongoose = require('mongoose');
+
+// get all drafts
+const getDrafts = async (req, res) => {
+    const drafts = await Drafts.find( {} ).sort({ createdAt: -1 });
+
+    // send drafts
+    res.status(200).json(drafts);
+}
+
+// get a single draft
+const getDraft = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: "No such draft"});
+    }
+    
+    const draft = await Drafts.findById(id);
+
+    if ( ! draft ){
+        return res.status(400).json({error: 'No such draft'});
+    }
+ 
+    res.status(200).json(draft);
+}
+
+// create a new draft
+const createDraft = async (req, res) => {
+    const { title, issue, content } = req.body;
+
+    // add draft to db
+    try{
+        const draft = await Drafts.create({title, issue, content});
+        res.status(200).json(draft);
+    } catch(error) {
+        res.status(400).json({error: error.message})
+    }
+}
+
+// delete a draft
+const deleteDraft = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: "No such draft"});
+    }
+
+    const draft = await Drafts.findOneAndDelete({_id: id});
+
+    if ( ! draft ){
+        return res.status(400).json({error: 'No such draft'});
+    }
+
+    res.status(200).json(draft);
+}
+
+// update a draft
+const updateDraft = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: "No such draft"});
+    }
+
+    const draft = await Drafts.findOneAndUpdate({_id: id}, {
+        ...req.body
+    })
+
+    if ( ! draft ){
+        return res.status(400).json({error: 'No such draft'});
+    }
+
+    res.status(200).json(draft);
+}
+
+module.exports = { getDrafts, getDraft, createDraft, deleteDraft, updateDraft };
+
--- backend/routes/drafts.js
+++ backend/routes/drafts.js
@@ -1,39 +1,22 @@
 const express = require('express');
-const Drafts = require('../models/draftModel'); 
+
+const  { getDrafts, getDraft, createDraft, deleteDraft, updateDraft } = require('../controllers/draftController');
 
 const router = express.Router();
 
 // GET all drafts
-router.get('/', (req, res) => {
-    res.json({message: 'GET drafts'})
-});
+router.get('/', getDrafts);
 
 // GET a single draft
-router.get('/:id', (req, res) => {
-    res.json({message: 'GET single drafts'})
-});
+router.get('/:id', getDraft);
 
 // POST a new draft
-router.post('/', async (req, res) => {
-    const {title, issue, content } = req.body;
-
-    try{
-        const draft = await Drafts.create({title, issue, content});
-        res.status(200).json(draft);
-    } catch(error) {
-        res.status(400).json({error: error.message})
-    }
-});
+router.post('/', createDraft);
 
 // DELETE a draft
-router.delete('/:id', (req, res) => {
-    res.json({message: 'DELETE a draft'})
-});
+router.delete('/:id', deleteDraft);
 
 // UPDATE a draft
-router.patch('/:id', (req, res) => {
-    res.json({message: 'UPDATE a draft'})
-});
-
+router.patch('/:id', updateDraft);
 
 module.exports = router;
